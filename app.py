b03c168a1d692f5e118ff9b381cbd34db6aea004
sahibzadakashif <kashifsahibzada88@gmail.com>
2025-04-06 17:54:43 +0500

    app.py

--- app.py
+++ app.py
@@ -77,8 +77,6 @@ with col3:
     left_logo, center, right_logo = st.columns([1, 2, 1])
     center.image("alzheimer.png", width=650)
     #right_logo.image("image.jpg", width=250)
-if __name__ == "__main__":
-    main()
 # Load model that was saved with only standard scikit-learn types
 model = joblib.load("model.pkl")
 
